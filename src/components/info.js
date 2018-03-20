8395e27c4f8ae5cb163733f7da2bb5f7ac40904b
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-03-19 19:38:46 -0600

    don't is better

--- src/components/info.js
+++ src/components/info.js
@@ -14,7 +14,7 @@ const Info = () => (
       <br />
       Please visit my <strong><a style={{ textDecoration: 'none', color: '#000' }} href="https://github.com/rojasleon">Github</a></strong> to see my work.
       <br />
-      I love teaching and learning; so if I can help you in something do not hesitate to contact me.
+      I love teaching and learning; so if I can help you in something don't hesitate to contact me.
     </AboutMe>
   </ContainerInfo>
 )
