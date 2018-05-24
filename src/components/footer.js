d08f9e1096349e344b9e0acdfaa472cb043e7491
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-05-23 20:24:15 -0500

    bug fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,9 +7,9 @@ const Footer = (props) => (
       <p className="by">© Juan Luis Rojas León - 2018</p>
     </div>
     <div className="social">
-      <i className="fab fa-github"></i>
-      <i className="fab fa-twitter"></i>
-      <i className="fab fa-linkedin-in"></i>
+      <a className="social-media" href="https://github.com/rojasleon"><i className="fab fa-github"></i></a>
+      <a className="social-media" href="https://twitter.com/rojas_leon_"><i className="fab fa-twitter"></i></a>
+      <a className="social-media" href="www.linkedin.com/in/rojasleon"><i className="fab fa-linkedin-in"></i></a>
     </div>
   </footer>
 );
