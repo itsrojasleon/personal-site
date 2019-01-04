a3d92be625dd0ce71ff7f3f665982420f8ead1a2
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-04 15:19:41 -0600

    without redux-logger

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import reducers from './reducers';
 
 const store = createStore(
   reducers,
-  applyMiddleware(thunk, logger),
+  applyMiddleware(thunk),
 );
 export default store;
