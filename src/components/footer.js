5f27e661cd25d8c2a5c1978ee9a951c2cea22042
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-28 18:02:30 -0600

    It works

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './index.css';
 
-const Footer = (props) => (
+const Footer = props => (
   <footer className="footer">
     <div>
       <p className="by">© Juan Luis Rojas León - 2018</p>
     </div>
     <div className="social">
-      <a className="social-media" href="https://github.com/rojasleon"><i className="fab fa-github"></i></a>
-      <a className="social-media" href="https://twitter.com/rojas_leon_"><i className="fab fa-twitter"></i></a>
-      <a className="social-media" href="www.linkedin.com/in/rojasleon"><i className="fab fa-linkedin-in"></i></a>
+      <a
+        className="social-media"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/rojasleon">
+        <i className="fab fa-github" />
+      </a>
+      <a
+        className="social-media"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://twitter.com/rojas_leon_">
+        <i className="fab fa-twitter" />
+      </a>
+      <a
+        className="social-media"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="www.linkedin.com/in/rojasleon">
+        <i className="fab fa-linkedin-in" />
+      </a>
     </div>
   </footer>
 );
--- src/containers/home.js
+++ src/containers/home.js
@@ -8,15 +8,26 @@ class Home extends Component {
         <h1 className="title">Juan Luis Rojas León</h1>
         <div>
           <p className="description">
-            I'm React / Frontend developer.
-            Please visit my <a className="github" href="https://github.com/rojasleon">Github</a> to see my work.
-            I love teaching and learning; so if I can help you in something don't hesitate to <a className="email" href="mailto:hello@luisrojas.me">contact me</a>.
+            I'm React / Frontend developer. Please visit my{' '}
+            <a
+              className="github"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/rojasleon">
+              Github
+            </a>{' '}
+            to see my work. I love teaching and learning; so if I can help you
+            in something don't hesitate to{' '}
+            <a className="email" href="mailto:rojasleon.dev@gmail.com">
+              contact me
+            </a>
+            .
           </p>
         </div>
         <div className="container-currently">
-          <span className="single-line"></span>
+          <span className="single-line" />
           <span className="currently">Currently developing open source</span>
-          <span className="single-line"></span>
+          <span className="single-line" />
         </div>
       </div>
     );
