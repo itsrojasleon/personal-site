6e910f32c5f0d5c73b582990ea4dafcce763d398
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-08 14:14:51 -0500

    Remove that comment

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Introduction from '../components/introduction/Introduction';
 
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary};
-// `;
-
 function Home() {
   return (
     <div>
