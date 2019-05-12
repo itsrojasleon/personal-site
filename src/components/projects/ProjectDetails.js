14e981478b6b2e83df97e4dbe2f547869cc65f84
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-12 02:27:00 -0500

    Only render when the mouse is on the image

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -9,17 +9,23 @@ import {
 } from '../styled-components/projects/project-details';
 
 function ProjectDetails(props) {
+  const [show, setShow] = React.useState(false);
   const imageEl = useRef(null);
   const { image, name, url } = props;
   const spans = useImage(imageEl);
   return (
-    <Container spans={spans}>
+    <Container
+      onMouseLeave={() => setShow(false)}
+      onMouseEnter={() => setShow(true)}
+      spans={spans}>
       <Img ref={imageEl} src={image} alt={name} />
-      <Content>
-        <Url>
-          <A>{url}</A>
-        </Url>
-      </Content>
+      {show && (
+        <Content>
+          <Url>
+            <A>{url}</A>
+          </Url>
+        </Content>
+      )}
     </Container>
   );
 }
