e34b16e0225560329a4190baf8ee9482a74de6bf
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-12 01:46:19 -0500

    Remove dense

--- src/components/styled-components/projects/projects.js
+++ src/components/styled-components/projects/projects.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   margin: auto;
-  margin-bottom: 140px;
+  margin-bottom: 180px;
 `;
 export const Wrapper = styled.div`
   width: 80%;
@@ -11,6 +11,5 @@ export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(236px, 1fr));
   grid-gap: 0 10px;
-  grid-auto-flow: dense;
   grid-auto-rows: 10px;
 `;
