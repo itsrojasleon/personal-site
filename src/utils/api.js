5339937ef3bcb19a5fb37713155b084ce75086bf
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-29 01:37:56 -0500

    search video

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,29 +7,36 @@ const projects = [
     name: 'Search Music v2',
     description: '',
     tecnologies: ['React with Hooks', 'Node', 'MongoDB', 'Redux'],
-    url: 'https://search-music.herokuapp.com/'
+    url: 'https://search-music.herokuapp.com/',
   },
   {
     image: `${base.url}/RTtpQWK.png`,
     name: 'Search Music v1',
     description: '',
     tecnologies: ['React with Hooks', 'Node', 'MongoDB', 'Redux'],
-    url: 'https://rojasleon.github.com/search-music'
+    url: 'https://rojasleon.github.com/search-music',
   },
   {
     image: `${base.url}/djOtu2N.png`,
     name: 'Emaily',
     description: '',
     tecnologies: ['React with Hooks', 'Node', 'MongoDB', 'Redux'],
-    url: 'https://server-with-node.herokuapp.com/'
+    url: 'https://server-with-node.herokuapp.com/',
   },
   {
     image: `${base.url}/b7lglYS.png`,
     name: 'Gifs and Stickers',
     description: '',
     tecnologies: ['React', 'Redux'],
-    url: 'https://search-infinite-gifs.surge.sh/'
-  }
+    url: 'https://search-infinite-gifs.surge.sh/',
+  },
+  {
+    image: `${base.url}/orU0T50.png`,
+    name: 'Search Video',
+    description: '',
+    tecnologies: ['React', 'Redux', 'Youtube API'],
+    url: 'https://search-video.surge.sh/',
+  },
 ];
 const handleProjects = () => {
   return new Promise((res, rej) => {
