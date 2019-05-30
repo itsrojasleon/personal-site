353dcf229af279eeec3fd1a73ad5b406990497ec
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-30 18:57:29 -0500

    A little margin

--- src/components/styled-components/whoIAm/who-i-am.js
+++ src/components/styled-components/whoIAm/who-i-am.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 80%;
   margin: 100px auto 100px auto;
+  @media (max-width: 769px) {
+    margin-top: 0;
+    margin-bottom: 180px;
+  }
 `;
 export const Char = styled.span`
   color: rgb(50, 158, 255);
