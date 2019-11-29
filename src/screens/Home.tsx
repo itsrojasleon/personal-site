460d797f2f681a8dffc61bdbd46aabbb516f5c85
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-11-29 15:14:16 -0600

    I don't need typescript

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import Introduction from '../components/introduction/Introduction';
 import { useDocumentTitle } from '../hooks/useDocumentTitle';
 
 const Home = (): JSX.Element => {
   useDocumentTitle('Juan Luis Rojas León');
+
   return (
     <div>
-      <Introduction />
+      <Suspense fallback="Loading...">
+        <Introduction />
+      </Suspense>
     </div>
   );
 };
